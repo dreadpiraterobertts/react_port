019ca5f243636d898ac2ee806a0d1adeb9cde4a1
Mussie <116425729+dreadpiraterobertts@users.noreply.github.com>
2025-04-14 11:11:21 +0300

    Update Footer.jsx

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <SocialMediaIcons/>
             <div className="md:flex justify-center md:justify-between text-center">
                 <p className="font-playfair font-semibold text-2xl text-yellow">Mussie Teklu</p>
-                <p className="font-playfair text-md text-yellow">©2024 Mussie. All rights reserved</p>
+                <p className="font-playfair text-md text-yellow">©2025 Mussie. All rights reserved</p>
             </div>
         </div>
     </footer>
